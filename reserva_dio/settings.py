d61cbf441bf8895e43fa8499c2e85c47ba938b2a
AlissonAzevedo <alissonazevedo13.1@gmail.com>
2022-03-24 10:51:00 -0300

    config the app

--- reserva_dio/settings.py
+++ reserva_dio/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['https://reservas-dio.herokuapp.com/']
+ALLOWED_HOSTS = ['reservas-dio.herokuapp.com']
 
 
 # Application definition
