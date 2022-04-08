eb56e841c32e2a70274d331b4232eeb388caaecb
AlissonAzevedo <61812126+AlissonAzevedo@users.noreply.github.com>
2022-04-08 08:39:40 -0300

    config app relatorios

--- reserva_dio/urls.py
+++ reserva_dio/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('relatorios/', include('relatorios.urls')),
     path('api/v1/', include('reserva.urls')),
     path('auth/', include('rest_framework.urls'))
 ]
