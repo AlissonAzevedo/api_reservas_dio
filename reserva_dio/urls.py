645a2d442412a37e7442e244eb6f3180d71288c7
Alisson Azevedo <alissonazevedo13.1@gmail.com>
2022-09-03 23:14:24 -0300

    chore: custom name site header and site title

--- reserva_dio/urls.py
+++ reserva_dio/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_title = "Reserva de Chaves"
+admin.site.site_header = 'Reserva de Chaves - Administração'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('relatorios/', include('relatorios.urls')),
